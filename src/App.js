fb219068189442220b94574d274d87fcfd6ad55b
James Mahan <36861942+jcmahan@users.noreply.github.com>
2018-06-27 18:23:03 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './Components/Header'
+
+
+class App extends Component {
+  render() {
+    return (
+      <Header />
+    );
+  }
+}
+
+export default App;
