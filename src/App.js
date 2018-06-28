7eefaa1eb7aabe1f92cffeb41a687b519d3a64db
James Mahan <36861942+jcmahan@users.noreply.github.com>
2018-06-28 15:53:44 -0700

    fixed ability to post

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header'
+import Post from './Components/Post'
 
 
 class App extends Component {
   render() {
     return (
-      <Header />
+      <div className="App">
+        <Header />
+        <section className='App-main'>
+        <Post nickname='Chris' avatar="https://www.laravelnigeria.com/img/chris.jpg" caption="Moving the community!" image="https://i.imgur.com/K82y6PV.png" />
+        <Post nickname="OG" avatar="https://www.laravelnigeria.com/img/chris.jpg" caption="Holding a mic" image="https://i.imgur.com/rNy0j9g.png" />
+        </section>
+      </div>
     );
   }
 }
